bd4ca93af871ce7782848d59f7e4a287e229f648
zilliah <kerry.z.smart@gmail.com>
2020-11-20 15:49:01 -0500

    Chaining operations mostly working. A couple of weird errors still.

--- calc.js
+++ calc.js
@@ -3,15 +3,14 @@ const opsBtns = document.querySelectorAll(".operators button");
 const currDisplay = document.querySelector(".curr");
 const equationDisplay = document.querySelector(".equation");
 let storedVal;  //the stored value being used in the current equation
-let currVal = 0; //the value current being input by user
+let currVal = 0; //the value currently being input by user
 let currOp; //the current operation
-let result = "a result hello"; //the result of the operation
+let result; //the result of the operation
 
 //accept number input
 numBtns.forEach(btn => {
     btn.addEventListener("click", event => {
         let num = btn.getAttribute("value");
-        console.log(num);
         currVal = currVal*10 + num*1;
         currDisplay.textContent = currVal; 
     });
@@ -21,39 +20,44 @@ numBtns.forEach(btn => {
 //accept operand input
 opsBtns.forEach(btn => {
     btn.addEventListener("click", event => {
+        if (currOp !== undefined) { //chaining operations
+            let newOp = btn.getAttribute("value");
+            storedVal = operate(currOp, storedVal, currVal);  //get result of prev thing
+            currVal = 0;
+            currOp = newOp;
+        } else if (result !== undefined) {  //prior input
+            storedVal = result;
+            result = undefined;
+        } else {
+            storedVal = currVal;
+            currVal = 0;
+        }
+        
         currOp = btn.getAttribute("value");
-
-        storedVal = currVal;
-        currVal = 0;
+        //update displays
         equationDisplay.textContent = storedVal + " " + btn.textContent;
         currDisplay.textContent = 0;
-        //TODO also need to call "operate" if this is part of an equn
+
     });
 });
 
 
-//clear - clears everything
-//maybe later can add a 1-click to clear curr val, 2-click to clear everything, but ignore this for now
-const clearBtn = document.querySelector("button[value='clear'");
-clearBtn.addEventListener("click", event => {
-    currDisplay.textContent = 0;
-
-    equationDisplay.textContent = 0; //eventually want to clear equation display, but creating styling problems atm
-    storedVal = undefined;
-    currVal = 0;
-    currOp = undefined;
-});
 
 
 //equals button evaluate
 const equalsBtn = document.querySelector("button[value='equals'");
 equalsBtn.addEventListener("click", event => {
     result = operate(currOp, storedVal, currVal);
-    if (result === "No operator") {
+    
+    if (result === "No operator") {  
         storedVal = currVal;
         currDisplay.textContent = currVal;
         equationDisplay.textContent = currVal;
         currVal = 0;
+        return;
+    } else if (result === "Error") {
+        window.alert("Error! Cannot divide by 0.");
+        clear();
         return;
     }
     //update displays
@@ -61,6 +65,7 @@ equalsBtn.addEventListener("click", event => {
     currDisplay.textContent = result;  
     currVal = 0;
     storedVal = result;
+    currOp = undefined;
 })
 //many clicking equals is bug
 
@@ -72,23 +77,36 @@ function operate(op, x, y) {
             return x + y;
         case "subt":
             return x - y;
-        case "mult": 
+            case "mult": 
             return x * y;
         case "div":
             return divide(x,y);
         default:
             return "No operator";
+            //should never get here! but its happening....
     }
 }
 
-
+        
 //divide - don't allow division by 0
-//todo need to allow add'l input here
 function divide(a, b) {
     if (currVal === 0) {
-        window.alert("Cannot divide by 0.");
         return "Error";
     } else {
         return a / b;
     }
+
+
+}
+//clear - clears everything
+const clearBtn = document.querySelector("button[value='clear'");
+clearBtn.addEventListener("click", event => clear());
+
+function clear() {
+    currDisplay.textContent = 0;
+    equationDisplay.textContent = 0; //eventually want to clear equation display, but creating styling problems atm
+    storedVal = undefined;
+    currVal = 0;
+    currOp = undefined;
+    result = undefined;
 }
